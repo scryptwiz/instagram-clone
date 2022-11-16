2d541251fc14398fc956e6db71a6c2499c18652c
eclaecosystem <68742854+Kelvide@users.noreply.github.com>
2022-11-16 08:16:33 +0100

    updated the header section

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,22 +1,71 @@
 import Image from "next/image";
 import Link from "next/link";
-import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
-import { BsInstagram, BsSearch } from 'react-icons/bs';
-import { FiSend } from 'react-icons/fi';
+import { AiFillHome, AiOutlineMenu, AiOutlineSend } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { CgProfile } from 'react-icons/cg';
+import { FiInstagram } from 'react-icons/fi';
+import { MdOutlineAddBox, MdOutlineExplore, MdOutlineFavoriteBorder } from 'react-icons/md';
 
 const Header = () => {
     return (
-        <div className="w-full lg:w-2/12 border-r border-gray-200 h-screen">
+        <div className="w-full md:w-fit lg:w-2/12 border-r border-gray-200 h-screen">
             {/* Large Screen Header */}
-            <header className="hidden lg:flex flex-col justify-between mx-auto px-3 py-5 gap-20">
+            <header className="hidden md:flex flex-col justify-between h-full mx-auto px-5 lg:py-7 md:py-5">
                 {/* Logo section */}
-                <div className="relative lg:inline-flex w-32 h-16 cursor-pointer">
+                <div className="relative hidden lg:inline-flex w-28 h-14 cursor-pointer">
                     <Image src="/logo.png" layout="fill" objectFit="contain" />
                 </div>
-                <div className="flex flex-col">
-                    <Link href='/' className="deskLink">
-                        <span>Home</span>
+                <div className="relative w-fit lg:hidden md:inline-flex cursor-pointer">
+                    <FiInstagram className="text-2xl text-black/80" />
+                </div>
+                <div className="flex flex-col gap-10 w-fit">
+                    <Link href='/' className="deskLink outline-none">
+                        <AiFillHome className="text-2xl text-black/80" />
+                        <span className="text-md hidden lg:flex font-semibold">Home</span>
                     </Link>
+                    <Link href='/' className="deskLink outline-none">
+                        <BiSearch className="text-2xl text-black/80" />
+                        <span className="text-md hidden lg:flex">Search</span>
+                    </Link>
+                    <Link href='/' className="deskLink outline-none">
+                        <MdOutlineExplore className="text-2xl text-black/80" />
+                        <span className="text-md hidden lg:flex">Explore</span>
+                    </Link>
+                    <Link href='/' className="deskLink outline-none">
+                        <AiOutlineSend className="text-2xl text-black/80" />
+                        <span className="text-md hidden lg:flex">Message</span>
+                    </Link>
+                    <Link href='/' className="deskLink outline-none">
+                        <MdOutlineFavoriteBorder className="text-2xl text-black/80" />
+                        <span className="text-md hidden lg:flex">Favourite</span>
+                    </Link>
+                    <Link href='/' className="deskLink outline-none">
+                        <MdOutlineAddBox className="text-2xl text-black/80" />
+                        <span className="text-md hidden lg:flex">Create</span>
+                    </Link>
+                    <Link href='/' className="deskLink outline-none">
+                        <CgProfile className="text-2xl text-black/80" />
+                        <span className="text-md hidden lg:flex">Profile</span>
+                    </Link>
+                </div>
+                <div className="flex">
+                    <Link href='/' className="deskLink outline-none">
+                        <AiOutlineMenu className="text-2xl text-black/80" />
+                        <span className="text-md hidden lg:flex">More</span>
+                    </Link>
+                </div>
+            </header>
+            {/* Small Screen Header */}
+            <header className="md:hidden flex justify-between py-2 px-3 w-full shadow">
+                <div className="relative hidden sm:inline-flex w-24 h-10 cursor-pointer">
+                    <Image src="/logo.png" layout="fill" objectFit="contain" />
+                </div>
+                <div className="flex gap-10 items-center">
+                    <div className="relative rounded">
+                        <BiSearch className="text-xl text-gray-500 absolute inset-y-0 my-auto ml-2" />
+                        <input type="text" className="rounded text-sm text-gray-500 bg-gray-200 border-none pl-8" placeholder="Search" />
+                    </div>
+                    <MdOutlineFavoriteBorder className="text-gray-500 text-2xl" />
                 </div>
             </header>
         </div>
